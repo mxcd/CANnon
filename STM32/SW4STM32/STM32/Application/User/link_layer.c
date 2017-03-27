6f4d563439daf1061cfd6afdc4204acd76694f51
Max Partenfelder <m.partenfelder@greenteam-stuttgart.de>
2017-03-27 12:26:16 +0200

    added jump to user app

--- STM32/SW4STM32/STM32/Application/User/link_layer.c
+++ STM32/SW4STM32/STM32/Application/User/link_layer.c
@@ -63,6 +63,49 @@ void jumpToUserApp()
 	GTBL_JumpToApplication();
 }
 
+void BL_RunUserApp(void) {
+
+	extern pFunction JumpToApplication;
+	extern uint32_t JumpAddress;
+
+	//Disable all Interrupts (ST PM0214 p.210)
+	NVIC->ICER[0] = (uint32_t)0xFFFFFFFF;
+	NVIC->ICER[1] = (uint32_t)0xFFFFFFFF;
+	NVIC->ICER[2] = (uint32_t)0xFFFFFFFF;
+	//Disable all interrupt pending flags (ST PM0214 p.212)
+	NVIC->ICPR[0] = (uint32_t)0xFFFFFFFF;
+	NVIC->ICPR[1] = (uint32_t)0xFFFFFFFF;
+	NVIC->ICPR[2] = (uint32_t)0xFFFFFFFF;
+
+	SysTick->CTRL = 0;
+
+	//Reset RCC clock
+	RCC->CR |= (uint32_t)0x00000001;
+	RCC->CFGR = (uint32_t)0x00000000;
+	RCC->CR &= (uint32_t)0xFEF6FFFF;
+	RCC->PLLCFGR = 0x24003010;
+	RCC->CR &= (uint32_t)0xFFFBFFFF;
+	RCC->CIR = 0x00000000;
+	FLASH->ACR = 0;
+
+
+	__ISB();
+	__DSB();
+
+	//basic test for valid application vector table start
+	if ((*(__IO uint32_t*)BL_UAStartAddress) == BL_UAStartPattern) {
+		// point to application reset handler (APPLICATION_ADDRESS +4 bytes)
+		JumpAddress = *(__IO uint32_t*) (BL_UAStartAddress + 4);
+		JumpToApplication = (pFunction) JumpAddress;
+
+		// update main stack pointer (MSP) register value
+		__set_MSP(*(__IO uint32_t*) BL_UAStartAddress);
+
+		// execute application code
+		JumpToApplication();
+	}
+}
+
 uint64_t getDeviceId()
 {
 	uint64_t devId;
